0cc49ffa958a97931e49d6a131b16d8266b22fcc
JAY-YANG1 <anrdl3294@naver.com>
2021-12-18 18:33:45 +0900

    초기세팅

---
+++ src/main/java/com/teamcloud/teamcloud/TeamcloudApplication.java
@@ -0,0 +1,13 @@
+package com.teamcloud.teamcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamcloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamcloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teamcloud/teamcloud/TeamcloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamcloud.teamcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamcloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
