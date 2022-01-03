edf6953dfc88fb2d9724d0e05e26f1c08e5380e8
JaeMukYang <anrdl3294@naver.com>
2022-01-03 14:17:02 +0900

    gitignore 재설정 회원가입 기능 일부

--- src/main/java/com/teamcloud/teamcloud/controller/MemberController.java
+++ src/main/java/com/teamcloud/teamcloud/controller/MemberController.java
@@ -1,5 +1,6 @@
 package com.teamcloud.teamcloud.controller;
 
+import com.teamcloud.teamcloud.domain.Member;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,8 +18,19 @@ public class MemberController {
         return null;
     }
 
-    @PostMapping("/members/createMember")
+    @GetMapping("/members/createMember")
     public String createMember() {
         return "/members/createMember";
+    }
+
+    @PostMapping("/members/createMember")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setEmail(form.getEmail());
+        member.setPassword(form.getPassword());
+
+        // memberService.join(member);
+
+        return "redirect:/";
     }
 }
