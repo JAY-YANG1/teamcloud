fda85790da809e55d77627f86fe3d94a34bc02a4
JaeMukYang <anrdl3294@naver.com>
2022-01-04 19:46:58 +0900

    member jpa 작업

---
+++ src/main/java/com/teamcloud/teamcloud/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package com.teamcloud.teamcloud.repository;
+
+import com.teamcloud.teamcloud.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}
