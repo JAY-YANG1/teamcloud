57d68a8c270849b552639a8ac313cd85897c279e
JaeMukYang <anrdl3294@naver.com>
2022-01-09 20:16:08 +0900

    게시판 작업2

--- src/main/java/com/teamcloud/teamcloud/repository/SpringDataJpaBoardRepository.java
+++ src/main/java/com/teamcloud/teamcloud/repository/SpringDataJpaBoardRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SpringDataJpaBoardRepository extends JpaRepository<TeamBoard, Long>, BoardRepository {
+public interface SpringDataJpaBoardRepository extends JpaRepository<TeamBoard, Long> , BoardRepository {
 }
