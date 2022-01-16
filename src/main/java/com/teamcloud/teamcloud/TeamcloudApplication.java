63ae637a2609245d75b34f28ddfd931b9ecd9558
JaeMukYang <anrdl3294@naver.com>
2022-01-16 19:11:37 +0900

    커맨드라인러너 데이터입력

--- src/main/java/com/teamcloud/teamcloud/TeamcloudApplication.java
+++ src/main/java/com/teamcloud/teamcloud/TeamcloudApplication.java
@@ -1,7 +1,14 @@
 package com.teamcloud.teamcloud;
 
+import com.teamcloud.teamcloud.domain.TeamBoard;
+import com.teamcloud.teamcloud.repository.BoardRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
+import java.util.stream.IntStream;
 
 
 @SpringBootApplication
@@ -11,5 +18,17 @@ public class TeamcloudApplication {
         SpringApplication.run(TeamcloudApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner runner(BoardRepository boardRepository) throws Exception {
+        return (args) -> {
+            IntStream.rangeClosed(1, 100).forEach(index ->
+                    boardRepository.save(TeamBoard.builder()
+                            .title("게시글" + index)
+                            .contents("내용" + index)
+                            .regdate(LocalDateTime.now())
+                            .updatedDate(LocalDateTime.now()).build()));
+        };
+    }
 
 }
+
