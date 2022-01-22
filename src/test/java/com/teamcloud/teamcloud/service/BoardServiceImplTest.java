40e34c3087b015284db2841c439fa28db05dd6c7
JaeMukYang <anrdl3294@naver.com>
2022-01-22 16:01:35 +0900

    BoardService Test 작성

---
+++ src/test/java/com/teamcloud/teamcloud/service/BoardServiceImplTest.java
@@ -0,0 +1,44 @@
+package com.teamcloud.teamcloud.service;
+
+import com.teamcloud.teamcloud.domain.TeamBoard;
+import com.teamcloud.teamcloud.repository.BoardRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+
+@SpringBootTest
+@Transactional
+class BoardServiceImplTest {
+
+    @Autowired
+    BoardService boardService;
+
+    @Autowired
+    BoardRepository boardRepository;
+
+
+    @Test
+    public void 게시글번호로찾기() throws Exception {
+        //given
+        TeamBoard teamBoard = TeamBoard.builder()
+                .userid("testId")
+                .title("testTitle")
+                .contents("testContents")
+                .views(3)
+                .likes(3)
+                .regdate(LocalDateTime.now())
+                .updatedDate(LocalDateTime.now()).build();
+        //when
+        TeamBoard findBoard = boardService.findBoardByBdno(teamBoard.getBdno());
+
+        //then
+        Assertions.assertEquals(teamBoard.getBdno(), findBoard.getBdno());
+    }
+
+
+
+}
